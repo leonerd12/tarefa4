476ef49f8e996d5eca97c2bdb83682ae30ce6e97
Leonardo Augusto <leonerd12@gmail.com>
2014-09-16 09:34:10 -0400

    [UPDATE] Adicionou-se máscaras de telefone para ficar mais amigável ao usuário.
    Signed-off-by: Leonardo Augusto <leonerd12@gmail.com>

--- form_func.php
+++ form_func.php
@@ -1,5 +1,29 @@
 <?php include "permissao_incluir.php";?>
 <?php include "top.php"; ?>
+<script type="text/javascript">
+
+function valPHONE(e,campo){
+    var tecla=(window.event)?event.keyCode:e.which;
+    if((tecla > 47 && tecla < 58 )){
+    	   mascara(campo, '(##)####-####');
+    	   return true;
+    	 }
+    else{
+    if (tecla != 8 ) return false;
+    else return true;
+    }
+}
+
+function mascara(src, mask){
+   var i = src.value.length;
+   var saida = mask.substring(1,2);
+   var texto = mask.substring(i);
+   if (texto.substring(0,1) != saida)
+   {
+      src.value += texto.substring(0,1);
+   }
+}
+</script>
 
 <form class="form-group" role="form" method="post" action="cadastro_func.php">
     <div class="form-group col-md-8">
@@ -15,8 +39,11 @@
         <input type="Email" class="form-control" name="email" placeholder="Email">
     </div>
     <div class="form-group col-md-8">
-        <label for="exampleInputPassword1">Telefone</label>
-        <input type="text" class="form-control" name="telefone" placeholder="Telefone">
+        <label for="exampleInputEmail1">Telefone</label>
+        <input class="form-control" type="text" 
+                             name="telefone" 
+                             onkeypress="return valPHONE(event,this); return false;" 
+                             maxlength="13" />
     </div>
     <div class="form-group">
         <div class="col-sm-10">
--- update_func.php
+++ update_func.php
@@ -12,6 +12,31 @@ $seleciona = $conn->query("SELECT * FROM funcionarios WHERE id_func = $id");
 $row = $seleciona->fetch(PDO::FETCH_OBJ);
 ?>
 
+<script type="text/javascript">
+
+function valPHONE(e,campo){
+    var tecla=(window.event)?event.keyCode:e.which;
+    if((tecla > 47 && tecla < 58 )){
+    	   mascara(campo, '(##)####-####');
+    	   return true;
+    	 }
+    else{
+    if (tecla != 8 ) return false;
+    else return true;
+    }
+}
+
+function mascara(src, mask){
+   var i = src.value.length;
+   var saida = mask.substring(1,2);
+   var texto = mask.substring(i);
+   if (texto.substring(0,1) != saida)
+   {
+      src.value += texto.substring(0,1);
+   }
+}
+</script>
+
 <form class="form-group" role="form" method="post" action="atualizacao_func.php">
     <div class="form-group col-md-8">
         <input type="hidden" name="id" value="<?= $row->id_func; ?>" >
@@ -28,7 +53,10 @@ $row = $seleciona->fetch(PDO::FETCH_OBJ);
     </div>
     <div class="form-group col-md-8">
         <label for="exampleInputPassword1">Telefone</label>
-        <input type="text" class="form-control" name="telefone" value="<?= $row->telefone_func; ?>">
+        <input class="form-control" type="text" 
+                             name="telefone" 
+                             onkeypress="return valPHONE(event,this); return false;" 
+                             maxlength="13" value="<?= $row->telefone_func; ?>">
     </div>
     <div class="form-group">
         <div class="col-sm-10">
